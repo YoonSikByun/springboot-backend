a01ba4fc4f75bb981aa18faa575be174e7dc0ce9
YS <yunsikpyun@gmail.com>
2023-12-20 17:50:23 +0900

    파일 추가

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            UserEntity user = UserEntity.builder().email(userDTO.getEmail()).username(userDTO.getUsername()).password(userDTO.getPassword()).build();
+
+            UserEntity registeredUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder().email(registeredUser.getEmail()).id(registeredUser.getId()).username(registeredUser.getUsername()).build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            ResponseDTO<UserDTO> responseDTO = ResponseDTO.<UserDTO>builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+
+        if(user != null) {
+            final String token = tokenProvider.create(user);
+            final UserDTO responseUserDTO = UserDTO.builder().email(user.getEmail()).id(user.getId()).token(token).build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else {
+            ResponseDTO<UserDTO> responseDTO = ResponseDTO.<UserDTO>builder().error("Login failed.").build();
+
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
